5ba7c6e839e0867a81f2b3d4c3a65fb37af56714
root <root@LAPTOP-542O0LA9>
2024-10-01 21:15:30 +0500

    Correct notes 2-th homework

--- simple-chat/src/index.js
+++ simple-chat/src/index.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
     const input = document.querySelector('.form-input');
     const chatContainer = document.querySelector('.chat-container');
+    const sendMessage = document.querySelector('.send-button')
 
     function loadMessages() {
         const messages = JSON.parse(localStorage.getItem('messages')) || [];
@@ -15,10 +16,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function displayMessage(message) {
         const messageElement = document.createElement('div');
-        messageElement.classList.add('message');
+        const sender = message.sender === 'You'? 'you' : 'opponent';
+        messageElement.classList.add('message', sender);
         messageElement.innerHTML = `
-            <strong>${message.sender}</strong> <small>${message.time}</small>
-            <p>${message.text}</p>
+            <strong class="message-text">${message.sender}</strong> <small class="message-text">${message.time}</small>
+            <p class="message-text">${message.text}</p>
         `;
         chatContainer.appendChild(messageElement);
         chatContainer.scrollTop = chatContainer.scrollHeight;
@@ -30,8 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem('messages', JSON.stringify(messages));
     }
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
+    function sendMessageHandler() {
         const messageText = input.value.trim();
         if (!messageText) return;
         const message = {
@@ -42,6 +43,25 @@ document.addEventListener('DOMContentLoaded', () => {
         saveMessage(message);
         displayMessage(message);
         input.value = '';
+
+        setTimeout(() => {
+            const message = {
+                sender: 'User',
+                text: 'I do not understand you(',
+                time: new Date().toLocaleString(),
+            };
+            saveMessage(message);
+            displayMessage(message);
+        }, 1000);
+    }
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        sendMessageHandler();
+    });
+
+    sendMessage.addEventListener('click', (event) => {
+        sendMessageHandler();
     });
 
     loadMessages();
