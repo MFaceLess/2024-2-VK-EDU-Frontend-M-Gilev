427ff1fff630d90367121ea8252729de7ffa86d0
root <root@LAPTOP-542O0LA9>
2024-10-13 21:17:56 +0500

    Сделал 4-ю домашку, высылаю на проверку

--- simple-chat/src/messanger.js
+++ simple-chat/src/messanger.js
@@ -1,4 +1,4 @@
-import './messanger.css';
+// import './messanger.css';
 
 document.addEventListener('DOMContentLoaded', function() {
     const chooseUserContainer = document.querySelector('.users-container');
@@ -27,13 +27,27 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     function displayMessages(friend) {
+        const randomNumber = Math.floor(Math.random() * 100);
+        const badge = randomNumber === 0 ? '✔️✔️' : randomNumber;
+
         const chatElement = document.createElement('div');
+
         chatElement.innerHTML = `
         <a href="index.html?user=${friend.whom}" class="message-link">
-            <strong class="user">${friend.whom}</strong> <small class="message-text">${friend.time}</small>
-            <p class="message-text">${friend.lastMessage}</p>
+            <div class="user-info">
+                <svg class="avatar" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-390q-21 0-35.5-14.5T310-440q0-21 14.5-35.5T360-490q21 0 35.5 14.5T410-440q0 21-14.5 35.5T360-390Zm240 0q-21 0-35.5-14.5T550-440q0-21 14.5-35.5T600-490q21 0 35.5 14.5T650-440q0 21-14.5 35.5T600-390ZM480-160q134 0 227-93t93-227q0-24-3-46.5T786-570q-21 5-42 7.5t-44 2.5q-91 0-172-39T390-708q-32 78-91.5 135.5T160-486v6q0 134 93 227t227 93Zm0 80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm-54-715q42 70 114 112.5T700-640q14 0 27-1.5t27-3.5q-42-70-114-112.5T480-800q-14 0-27 1.5t-27 3.5ZM177-581q51-29 89-75t57-103q-51 29-89 75t-57 103Zm249-214Zm-103 36Z"/></svg>
+                <div class="user-details">
+                    <div class="top-container">
+                        <strong class="user">${friend.whom}</strong> 
+                        <small class="time-text">${friend.time}</small>
+                    </div>
+                    <small class="message-text">${friend.lastMessage}</small>
+                </div>
+            </div>
+            <div class="badge">${badge}</div>
         </a>
         `;
+
         chatContainer.appendChild(chatElement);
         chatContainer.scrollTop = chatContainer.scrollHeight;
     }
@@ -55,13 +69,14 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     
         if (existingMessage) {
-            existingMessage.innerHTML = `
-                <strong class="message-text">${friend.whom}</strong> <small class="message-text">${friend.time}</small>
-                <p class="message-text">${friend.lastMessage}</p>
-            `;
-        } else {
-            displayMessages(friend);
-        }
+            existingMessage.remove();
+            // existingMessage.innerHTML = `
+            //     <strong class="message-text">${friend.whom}</strong> <small class="message-text">${friend.time}</small>
+            //     <p class="message-text">${friend.lastMessage}</p>
+            // `;
+        } /*else {*/
+        displayMessages(friend);
+        // }
     }
 
     //Проверка обновления чата
