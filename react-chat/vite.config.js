645e225bc1a81122e9dd81bb67a79df612faa1a4
root <root@LAPTOP-542O0LA9>
2024-10-26 20:50:39 +0500

    Сделал 5-ю домашку, посылаю на проверку, вся функциональность переведена на React

--- react-chat/vite.config.js
+++ react-chat/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({server: {
+  base: '2024-2-VK-EDU-Frontend-M-Gilev',
   watch: {
     usePolling: true,
   },
