0101eb7fbe949730645c1c373456a1941a73f38f
root <root@LAPTOP-542O0LA9>
2024-10-26 20:54:09 +0500

    Сделал 5-ю домашку, посылаю на проверку, вся функциональность переведена на React

--- react-chat/vite.config.js
+++ react-chat/vite.config.js
@@ -2,11 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({server: {
-  base: '2024-2-VK-EDU-Frontend-M-Gilev',
-  watch: {
-    usePolling: true,
-  },
-},
-  plugins: [react()],
+export default defineConfig({
+    plugins: [react()],
+    base: '2024-2-VK-EDU-Frontend-M-Gilev',
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({server: {
+//     base: '2024-2-VK-EDU-Frontend-M-Gilev',
+//     watch: {
+//       usePolling: true,
+//     },
+//   },
+//     plugins: [react()],
+// })
