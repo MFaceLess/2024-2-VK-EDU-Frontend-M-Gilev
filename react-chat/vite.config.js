7fec740d8c155451bba63fd352fe331fca5e98c3
root <root@LAPTOP-542O0LA9>
2024-10-26 20:59:12 +0500

    Сделал 5-ю домашку, посылаю на проверку, вся функциональность переведена на React

--- react-chat/vite.config.js
+++ react-chat/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '2024-2-VK-EDU-Frontend-M-Gilev',
+    base: '/2024-2-VK-EDU-Frontend-M-Gilev',
 })
 
 
