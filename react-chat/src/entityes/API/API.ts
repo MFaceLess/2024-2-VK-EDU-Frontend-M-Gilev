f688d056832911cc190621cdfcdabae91c9167d9
root <root@LAPTOP-542O0LA9>
2024-12-23 19:55:45 +0500

    Создан класс для работы с API

---
+++ react-chat/src/entityes/API/API.ts
@@ -0,0 +1,54 @@
+import { ErrorResponse } from "react-router-dom";
+import { toast } from "react-toastify";
+
+export class API {
+    private baseURL: string;
+
+    constructor(baseURL: string) {
+        this.baseURL = baseURL;
+    }
+
+    private async call<T>(  endpoint: string, 
+                            method: 'GET' | 'POST' | 'PUT' | 'DELETE', 
+                            body?: unknown, 
+                            headers?: HeadersInit) : Promise<T> {
+        try {
+            const options: RequestInit = {
+                method,
+                body: body instanceof FormData ? body : body ? JSON.stringify(body) : undefined,
+            };
+
+            if (headers) {
+                options.headers = headers;
+            }
+            const response = await fetch(`${this.baseURL}${endpoint}`, options);
+
+            if (!response.ok) {
+                const error: ErrorResponse = await response.json();
+                throw error;
+            }
+
+            return await response.json();
+        } catch(error) {
+            // toast.error(error.toString());
+            throw error;
+        }
+    }
+
+    public get<T>(endpoint: string): Promise<T> {
+        return this.call<T>(endpoint, 'GET');
+    }
+
+    public post<T>(endpoint: string, body: unknown, isFormData = false): Promise<T> {
+        const headers = isFormData ? undefined : { 'Content-Type' : 'application/json' };
+        return this.call<T>(endpoint, 'POST', body, headers);
+    }
+
+    public put<T>(endpoint: string, body: unknown): Promise<T> {
+        return this.call<T>(endpoint, 'PUT', body);
+    }
+
+    public delete<T>(endpoint: string): Promise<T> {
+        return this.call<T>(endpoint, 'DELETE');
+    }
+}
--- react-chat/src/entityes/API/auth/fetchRegister/index.ts
+++ react-chat/src/entityes/API/auth/fetchRegister/index.ts
@@ -1,17 +1,31 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { Enviroment } from '../../../Enviroment/Enviroment';
+import { API } from '../../API';
+import { toast } from 'react-toastify';
+
+const api = new API(Enviroment.baseURL);
 
 export const fetchRegister = createAsyncThunk(
     'auth-form/fetchRegister',
-    async (formData: FormData) => {
-        const response = await fetch(`${Enviroment.registerEndpoint}`, {
-            method: 'POST',
-            body: formData,
-        });
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(JSON.stringify(errorData));
+    async (formData: FormData, { rejectWithValue }) => {
+        try {
+            const response = await api.post(Enviroment.registerEndpoint, formData, true);
+            return response;
+        } catch (error) {
+            const keys = Object.keys(error);
+            toast.error(error[keys[0]][0]);
+            throw new Error(JSON.stringify(error));
+            // return rejectWithValue(error);
         }
-        return await response.json();
+
+        // const response = await fetch(`${Enviroment.registerEndpoint}`, {
+        //     method: 'POST',
+        //     body: formData,
+        // });
+        // if (!response.ok) {
+        //     const errorData = await response.json();
+        //     throw new Error(JSON.stringify(errorData));
+        // }
+        // return await response.json();
     }
 );
