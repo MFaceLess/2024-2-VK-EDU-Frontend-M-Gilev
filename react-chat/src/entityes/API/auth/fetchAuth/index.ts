6b5248e9b0c2c97d8a105cb6038ccafc60eed923
root <root@LAPTOP-542O0LA9>
2025-01-05 21:12:14 +0500

    Исправление деплоя

--- react-chat/src/entityes/API/auth/fetchAuth/index.ts
+++ react-chat/src/entityes/API/auth/fetchAuth/index.ts
@@ -1,24 +1,27 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { Enviroment } from '../../../Enviroment/Enviroment';
+import { API } from '../../API';
+import { toast } from 'react-toastify';
+
+
+interface AuthResponse {
+    refresh:    string;
+    access:     string;
+}
+const api = new API(Enviroment.baseURL);
 
 export const fetchAuth = createAsyncThunk(
     'auth-form/fetchAuth',
     async (args: {username: string, password: string}) => {
         const { username, password } = args;
-        const response = await fetch(`${Enviroment.authEndpoint}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({username, password})
-        });
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(JSON.stringify(errorData));
+        try {
+            const response: AuthResponse = await api.post<AuthResponse>(`${Enviroment.authEndpoint}`, {username, password})
+            localStorage.setItem('refresh', response.refresh);
+            localStorage.setItem('access', response.access);
+        } catch (error) {
+            const keys = Object.keys(error);
+            toast.error(error[keys[0]]);
+            throw new Error(JSON.stringify(error));
         }
-        const data = await response.json()
-        localStorage.setItem('refresh', data.refresh);
-        localStorage.setItem('access', data.access);
-        return data;
     }
 )
